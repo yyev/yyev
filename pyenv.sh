55219be6fcedd6c9c1af06f98b495e133b932a03
yyev <yyevonoz@gmail.com>
2020-04-14 18:41:56 +0900

    add libffi-dev install for solving ModuleNotFoundError: No module named '_ctypes'

--- pyenv.sh
+++ pyenv.sh
@@ -21,6 +21,7 @@ function install()
 {
     sudo apt-get update
     sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
+    sudo apt-get install libffi-dev
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     init_bash_profile
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/${target_profile_name}
