283e8ff3aa01f351d6cfd317f97013a351459705
yyev <yyev>
2023-11-15 00:52:56 +0900

    add install liblzma-dev, python3-tk for python 3.11.x

--- pyenv.sh
+++ pyenv.sh
@@ -22,6 +22,8 @@ function install()
     sudo apt-get update
     sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
     sudo apt-get install libffi-dev
+    sudo apt install liblzma-dev
+    sudo apt-get install python3-tk
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     init_bash_profile
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/${target_profile_name}
